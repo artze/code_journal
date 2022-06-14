6afd55f89853e998046b5e28c261d17cce24c934
artze <arthurwtm@gmail.com>
2022-06-14 21:23:29 +0800

    Add goatcounter script

--- journal/.vuepress/config.js
+++ journal/.vuepress/config.js
@@ -1,14 +1,24 @@
 module.exports = {
   title: 'Code Journal',
   themeConfig: {
-    nav: [{ text: 'Tags', link: '/tags/' }]
+    nav: [{ text: 'Tags', link: '/tags/' }],
   },
+  head: [
+    [
+      'script',
+      {
+        'data-goatcounter': 'https://artzecodejournal.goatcounter.com/count',
+        async: true,
+        src: '//gc.zgo.at/count.js',
+      },
+    ],
+  ],
   plugins: [
     [
       '@vuepress/google-analytics',
       {
-        ga: ' UA-106933848-3'
-      }
-    ]
-  ]
+        ga: ' UA-106933848-3',
+      },
+    ],
+  ],
 };
