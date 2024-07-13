37e3d9d7dde686c4d308cbd31984af67e2a36991
artze <13703523+artze@users.noreply.github.com>
2024-07-13 12:16:33 +0800

    Update `.eslintrc` and `.prettierrc`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,14 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": [
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
-    "prettier/vue"
+  extends: [
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
-  "parserOptions": {
-    "ecmaVersion": 2018
+  parserOptions: {
+    ecmaVersion: 2018,
   },
-  "rules": {
-    "prettier/prettier": ["error", {
-      "tabWidth": 2,
-      "singleQuote": true,
-      "arrowParens": "always",
-      "trailingComma": "none"
-    }]
-  }
 };
