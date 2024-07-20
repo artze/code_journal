5e46cf98703fbff38a45512e2b21d75cb445bf6f
artze <13703523+artze@users.noreply.github.com>
2024-07-20 20:20:52 +0800

    Update `add-post.sh`

--- add_post.sh
+++ add_post.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-filename=""
-if [ "$1" = "-o" ]; then
-  filename=$2
-else
-  filename=$1
-fi
+filename=$1
 
 cat << EOF > ./journal/posts/${filename}.md
 ---
@@ -17,11 +12,4 @@ timestamp: `date +%s%N | cut -b1-13`
 
 # 
 
-
-<PostDate />
-<PageTags />
 EOF
-
-if [ "$1" = "-o" ]; then
-  subl ./journal/posts/$2.md
-fi
